4c9136ae9ab67c2f99381333ab007e749eab28f5
Bun <hoangquocnam021117@gmail.com>
2023-06-11 19:09:22 +0700

    Fix config

--- app.config.ts
+++ app.config.ts
@@ -23,12 +23,16 @@ const config: ExpoConfig = {
       foregroundImage: "./src/assets/adaptive-icon.png",
       backgroundColor: "#FFFFFF",
     },
+    package: "com.udt.mobile.timesheet",
   },
   web: {
     favicon: "./src/assets/favicon.png",
   },
   extra: {
     API_URL: process.env.API_URL,
+    eas: {
+      projectId: "52e1cfe1-5600-4a99-948d-2dde3345a8f8",
+    },
   },
 };
 
