a091463d72c32b53dd214a3d1cbb6d2b37d5585a
Bun <hoangquocnam021117@gmail.com>
2023-06-12 10:31:39 +0700

    native base

--- App.tsx
+++ App.tsx
@@ -3,17 +3,20 @@ import { StatusBar } from "expo-status-bar";
 import { ToastProvider } from "./src/context/toast.context";
 import BottomToast from "./src/components/UI/BottomToast";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { NativeBaseProvider } from "native-base";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <AppNavigation />
-        <StatusBar style="auto" />
-        <BottomToast />
-      </ToastProvider>
-    </QueryClientProvider>
+    <NativeBaseProvider>
+      <QueryClientProvider client={queryClient}>
+        <ToastProvider>
+          <AppNavigation />
+          <StatusBar style="auto" />
+          <BottomToast />
+        </ToastProvider>
+      </QueryClientProvider>
+    </NativeBaseProvider>
   );
 }
